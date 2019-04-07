63bd52df71f0ab8cbf279b6bc0e0ecef7f54fd12
CHOCKALINGAM SUBRAMANIAN <checkoutfood.contact@gmail.com>
2019-04-07 23:05:21 +0100

    deployment

---
+++ config/database.js
@@ -0,0 +1,10 @@
+const crypto = require('crypto').randomBytes(256).toString('hex'); // Provides cryptographic functionality (OpenSSL's hash, HMAC, cipher, decipher, sign and verify functions)
+
+// Export config object
+module.exports = {
+//  uri: process.env.databaseUri, // Databse URI and database name
+  uri: "mongodb://checkoutfood:checkoutfood123@cluster0-5ffrd.mongodb.net/test?retryWrites=true",
+  secret: crypto, // Cryto-created secret
+  db: 'test'
+
+}
